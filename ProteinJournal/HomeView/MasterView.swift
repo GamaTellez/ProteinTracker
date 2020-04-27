a15b3bd0ecdc8eb4112264789315f977f7c0613d
Gama Tellez <gamatellez@gmail.com>
2020-04-27 16:04:00 -0600

    Modified view to display the notification banner

--- ProteinJournal/HomeView/MasterView.swift
+++ ProteinJournal/HomeView/MasterView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 
 struct MasterView: View {
     @State private var settingsViewPresented = false
-    @State private var analytivsViewPresented = false
+    @State private var analyticsViewPresented = false
     @State private var newEntryMenuViewShowing = false
     @State private var manualEntryViewShowing = false
+    @State private var newEntryBannerShouldShow = false
     
     @State private var newJournalEntry : JournalEntry?
     
@@ -31,11 +32,11 @@ struct MasterView: View {
                     Spacer()
                     DayText(text:"Monday March 20th")
                     Spacer()
-                    BarButton(imageName: "analytics", function: { self.analytivsViewPresented.toggle() }, side: .trailing, paddingSize: 10)
-                }//HSTACK 1
-                    .frame(minWidth: nil, idealWidth: nil, maxWidth: .infinity, minHeight: 50, idealHeight: nil, maxHeight: 50, alignment: .leading)
-                    .background(Color.black)
-                    .padding(.top, 34)
+                    BarButton(imageName: "analytics", function: { self.analyticsViewPresented.toggle() }, side: .trailing, paddingSize: 10)
+                }
+                .frame(minWidth: nil, idealWidth: nil, maxWidth: .infinity, minHeight: 50, idealHeight: nil, maxHeight: 50, alignment: .leading)
+                .background(Color.black)
+                .padding(.top, 34)
                 HStack {
                     ZStack {
                         GoalProgressView(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
@@ -49,7 +50,7 @@ struct MasterView: View {
                         }
                         GoalCountLabel(proteinCount: self.proteinCount, proteinGoal: self.proteinGoal)
                         if self.manualEntryViewShowing {
-                            ManualEntryView(isShowing: $manualEntryViewShowing, newJournalEntry: $newJournalEntry)
+                            ManualEntryView(isViewShowing: self.$manualEntryViewShowing, newJournalEntry: self.$newJournalEntry, newEntryBannerShouldShow: self.$newEntryBannerShouldShow)
                         }
                     }
                 }
@@ -59,13 +60,28 @@ struct MasterView: View {
                     CaloriesCircle(width: 110, height: 110, caloriesCount: 240)
                 }
                 Spacer()
-                
                 ZStack {
                     HStack {
+                        if self.newEntryBannerShouldShow {
+                            EntrySavedBanner(entry: JournalEntry(created: Date(), calories: 23, protein: 34, name: "Chicken Enchiladas"))
+                                .onAppear {
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                        withAnimation {
+                                            self.newEntryBannerShouldShow.toggle()
+                                        }
+                                    }
+                            }
+                        }
                         Spacer()
                         AddEntryMenuButton(function: { self.addEntryButtonTapped() }, imageName: "arrowUp", entryMenuShowing: self.newEntryMenuViewShowing)
                         if self.newEntryMenuViewShowing {
                             AddEntryMenuView(manualEntryFunction: self.manualEntryButtonTapped, searchEntryFuntion: self.searchEntryButtonTapped)
+                        }
+                    }.onAppear {
+                        if self.newJournalEntry != nil {
+                            withAnimation {
+                                self.analyticsViewPresented.toggle()
+                            }
                         }
                     }
                 }
@@ -74,14 +90,6 @@ struct MasterView: View {
                 .frame(minWidth: nil, idealWidth: nil, maxWidth: .infinity, minHeight: nil, idealHeight: nil, maxHeight: .infinity, alignment: .center)
                 .background(Color.viewControllersGrey).edgesIgnoringSafeArea(.all)
         }
-    }
-    
-    private func newDayButtonTapped() {
-        print("New day button tapped")
-    }
-    
-    private func newGoalTapped() {
-        print("New goal tapped")
     }
     
     private func addEntryButtonTapped() {
