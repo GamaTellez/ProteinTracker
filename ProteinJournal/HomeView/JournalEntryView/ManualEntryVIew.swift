7dee13af6f6b71578fe7d466f922893cbd24eb07
Gama Tellez <gamatellez@gmail.com>
2020-06-29 12:16:16 -0600

    A view where users can create a new entry to add to the protein goal

--- ProteinJournal/HomeView/JournalEntryView/ManualEntryVIew.swift
+++ ProteinJournal/HomeView/JournalEntryView/ManualEntryVIew.swift
@@ -8,87 +8,80 @@
 
 import SwiftUI
 
-
 struct ManualEntryView : View {
     
-    @ObservedObject private var entryChecker : EntryChecker = EntryChecker()
-    @Binding var isViewShowing : Bool
-    @Binding var newJournalEntry : JournalEntry?
-    @Binding var newEntryBannerShouldShow : Bool
-    @State private var showActivityView = false
+    @ObservedObject private var entryVeryfier : EntryVeryfier = EntryVeryfier()
+    //@Binding var isViewShowing : Bool
+//    @Binding var newJournalEntry : ProteinEntry?
+//    @Binding var newEntrySavedSuccessfully : Bool
+//    @State private var showActivityView = false
     
     var body : some View {
-        VStack {
-            Text("New Entry")
-                .foregroundColor(Color.white)
-                .font(Font.custom("StringHelveticaNeue-CondensedBold", size: 20))
-                .padding(10)
-                .frame(width: 280, height: 50, alignment: .center)
-            JournalEntryTextField(placeHolder: "Name", textBinding: $entryChecker.entryName, keyBoard: .default)
-            JournalEntryTextField(placeHolder: "Protein", textBinding: $entryChecker.entryProtein, keyBoard: .numberPad)
-            JournalEntryTextField(placeHolder: "Calories", textBinding: $entryChecker.entryCalories, keyBoard: .numberPad)
-            
-            if !self.showActivityView {
-                HStack {
-                    Spacer()
-                    VStack {
-                        Spacer()
-                        NewEntryButton(saveAction: self.cancelButtonTapped, entryChecker: self.entryChecker, type: .cancel)
-                        Spacer()
-                    }
-                    Spacer()
-                    VStack {
-                        Spacer()
-                        NewEntryButton(saveAction: self.saveButtonTapped, entryChecker: self.entryChecker, type: .save)
-                        Spacer()
-                    }
-                    Spacer()
+        VStack(alignment: .leading) {
+            HStack {
+                Button(action: { }) {
+                    Image("dismiss").foregroundColor(Color.white)
+                }
+                .padding(.trailing, 5)
+                Text("New Entry")
+                .foregroundColor(Color.foreGroundColor)
+                .font(AppFonts.title.of(size: 20))
+                Spacer()
+                if self.entryVeryfier.portionFilled == ProgressPortion.threeThirds {
+                    SaveEntryButton(saveAction: self.saveButtonTapped)
                 }
             }
-            if showActivityView {
-                HStack {
-                    ShowActivityView()
-                        .frame(width: 50, height: 50, alignment: .center)
-                        .foregroundColor(Color.white)
-                }
+            .frame(minWidth: nil, idealWidth: nil, maxWidth: .infinity, minHeight: 50, idealHeight: nil, maxHeight: 50, alignment: .leading)
+            .background(Color.black)
+            .padding(.top, 32)
+            ProgressBar(progressValue: self.$entryVeryfier.portionFilled).frame(height:20)
+            VStack(alignment: .leading) {
+                EntryTextField(text: self.$entryVeryfier.entryName, type: .name)
+                EntryTextField(text: self.$entryVeryfier.entryProtein, type: .protein)
+                EntryTextField(text: self.$entryVeryfier.entryCalories, type: .calories)
             }
             Spacer()
         }
-        .frame(width: 300, height: 280, alignment: .center)
-        .background(Color.black)
-        .cornerRadius(20)
-        .transition(AnyTransition.opacity.combined(with: .scale))
-        .onDisappear {
-            self.masterViewEntrySavedBannerShouldShow()
-        }
+        .frame(minWidth: nil, idealWidth: nil, maxWidth: .infinity, minHeight: nil, idealHeight: nil, maxHeight: .infinity, alignment: .leading)
+        .background(Color.backgroundGrey).edgesIgnoringSafeArea(.all)
+//        .onDisappear {
+//            self.savedBannerShouldShow()
+//        }
     }
     
-    private func masterViewEntrySavedBannerShouldShow() {
-        if self.newJournalEntry != nil {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                withAnimation {
-                    self.newEntryBannerShouldShow.toggle()
-                }
-            }
-        }
+    private func savedBannerShouldShow() {
+//        if self.newJournalEntry != nil {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+//                withAnimation {
+//                    self.newEntrySavedSuccessfully.toggle()
+//                }
+//            }
+//        }
     }
     
     private func cancelButtonTapped() {
         withAnimation {
-            self.isViewShowing.toggle()
+          //  self.isViewShowing.toggle()
         }
     }
     
     private func saveButtonTapped() {
-        self.showActivityView.toggle()
-        if let newJournalEntry = self.entryChecker.newEntry() {
-            self.newJournalEntry = newJournalEntry
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {//removew when actually saving to icloud
-                withAnimation {
-                    self.isViewShowing.toggle()
-                }
-            }
-        }
+//        self.showActivityView.toggle()
+//        if let newJournalEntry = self.entryChecker.saveNewEntry() {
+//            self.newJournalEntry = newJournalEntry
+//            
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {//removew when actually saving to icloud
+//                withAnimation {
+//                    self.isViewShowing.toggle()
+//                }
+//            }
+//        }
     }
 }
+
+struct ManualEntryPreview: PreviewProvider {
+    static var previews: some View {
+        ManualEntryView()
+    }
+}
+
