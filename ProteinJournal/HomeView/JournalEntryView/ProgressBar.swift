d5850f174b543799b40e0856f5a79a840f46bffe
Gama Tellez <gamatellez@gmail.com>
2020-06-29 12:31:26 -0600

    A custom button that appears and dissappears when all the required fields are filled or not

--- ProteinJournal/HomeView/JournalEntryView/ProgressBar.swift
+++ ProteinJournal/HomeView/JournalEntryView/ProgressBar.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2020 GamaTellez. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+enum ProgressPortion {
+    case none
+    case oneThird
+    case twoThirds
+    case threeThirds
+    
+    var value : Int {
+        switch self {
+        case .none:
+            return 0
+        case .oneThird:
+            return 1
+        case .twoThirds:
+            return 2
+        case .threeThirds:
+            return 3
+        }
+    }
+}
+
+struct ProgressBar : View {
+    @Binding var progressValue : ProgressPortion
+    
+    var body : some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .opacity(0.3)
+                    .foregroundColor(Color.white)
+                Rectangle()
+                    .frame(width: min((geometry.size.width / 3.0) *  CGFloat(self.progressValue.value), geometry.size.width), height: geometry.size.height)
+                    .foregroundColor(Color.appGreen)
+                    .animation(.linear)
+            }
+        }
+    }
+}
--- ProteinJournal/HomeView/JournalEntryView/SaveEntryButton.swift
+++ ProteinJournal/HomeView/JournalEntryView/SaveEntryButton.swift
@@ -8,25 +8,17 @@
 
 import SwiftUI
 
-enum NewEntryButtonType {
-    case save, cancel
-}
-
 struct SaveEntryButton : View {
     var saveAction : () -> Void
-    var entryChecker : EntryVeryfier
-    var type : NewEntryButtonType
-    
     var body : some View {
         Button(action: { self.saveAction() }) {
-            Text(self.type == .cancel ? "Cancel" : "Save")
+            Image("upload")
+                .padding(.trailing, 5)
         }
         .foregroundColor(Color.foreGroundColor)
-//        .disabled(self.type == .cancel ? self.entryChecker.isFormFilled : !self.entryChecker.isFormFilled)
-        .font(Font.custom("StringHelveticaNeue-CondensedBold", size: 20))
-//        .frame(width: self.type == .cancel ? (!self.entryChecker.isFormFilled ? 130 : 100) : (!self.entryChecker.isFormFilled ? 100 : 130), height: self.type == .cancel ? (!self.entryChecker.isFormFilled ? 50 : 30) : (!self.entryChecker.isFormFilled ? 30 : 50), alignment: .center)
+        .font(AppFonts.title.of(size: 20))
         .cornerRadius(10)
         .overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
-        .transition(AnyTransition.opacity.animation(.easeInOut(duration: 0.1)))
+        .transition(AnyTransition.opacity.animation(.easeInOut(duration: 0.5)))
     }
 }
