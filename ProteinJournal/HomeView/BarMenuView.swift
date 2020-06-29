3c05aa2fb66cc2e02c1bf480ebe6524e0176a4e3
Gama Tellez <gamatellez@gmail.com>
2020-06-29 11:55:05 -0600

    Moved all the code involving the top bar (acting as a nav bar) into it is own class to improve code readability

--- ProteinJournal/HomeView/BarMenuView.swift
+++ ProteinJournal/HomeView/BarMenuView.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2020 GamaTellez. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+struct BarMenuView : View {
+    @Binding var settingsViewPresented : Bool
+    @Binding var analyticsViewPresented : Bool
+    
+    var body : some View {
+        HStack {
+            BarButton(imageName: "settings", function: {
+                self.settingsViewPresented.toggle()
+                
+            }, side: .leading, paddingSize: 10)
+            .sheet(isPresented: $settingsViewPresented) { SettingsView()
+            }
+            Spacer()
+            DayText(text:"Monday March 20th")
+            Spacer()
+            BarButton(imageName: "analytics", function: { self.analyticsViewPresented.toggle()
+            }, side: .trailing, paddingSize: 10)
+        }
+        .frame(minWidth: nil, idealWidth: nil, maxWidth: .infinity, minHeight: 50, idealHeight: nil, maxHeight: 50, alignment: .leading)
+        .background(Color.black)
+        .padding(.top, 38)
+    }
+}
