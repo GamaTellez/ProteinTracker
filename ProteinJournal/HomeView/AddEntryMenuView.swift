abe0c4d09c6f2fbabfba72d36bd36e7707c0795b
Gama Tellez <gamatellez@gmail.com>
2021-01-14 17:17:54 -0700

    Code refactored for readability

--- ProteinJournal/HomeView/AddEntryMenuView.swift
+++ ProteinJournal/HomeView/AddEntryMenuView.swift
@@ -13,16 +13,25 @@ struct AddEntryMenuView : View {
     
     @Binding var addEntryMenuViewShowing : Bool
     
-    init(manualEntryViewShowing: Binding<Bool>, entryMenuViewShowing: Binding<Bool>) {
+    @Binding var alertShowing : Bool
+    
+    private var dayAvailable : Bool
+    
+    init(manualEntryViewShowing: Binding<Bool>, entryMenuViewShowing: Binding<Bool>, alertShowing: Binding<Bool>, dayAvailable: Bool) {
         self._manualEntryViewShowing = manualEntryViewShowing
         self._addEntryMenuViewShowing = entryMenuViewShowing
+        self._alertShowing = alertShowing
+        self.dayAvailable = dayAvailable
     }
     
     var body : some View {
         HStack {
             Spacer()
+            
             VStack {
+                
                 Spacer()
+                
                 HStack {
                     Text("Search entry")
                         .foregroundColor(Color.foreGroundColor)
@@ -37,15 +46,22 @@ struct AddEntryMenuView : View {
                     .foregroundColor(Color.foreGroundColor)
                     .overlay(Circle().stroke(Color.foreGroundColor, lineWidth: 0.5))
                 }
+                
                 Spacer()
+                
                 HStack {
                     Text("Manual entry")
                         .foregroundColor(Color.foreGroundColor)
                         .font(Font.custom("StringHelveticaNeue-CondensedBold", size: 15))
                     Button(action: {
                         withAnimation {
-                            self.manualEntryViewShowing.toggle()
-                            self.addEntryMenuViewShowing.toggle()
+                            if self.dayAvailable {
+                                self.manualEntryViewShowing.toggle()
+                                self.addEntryMenuViewShowing.toggle()
+                            } else {
+                                self.alertShowing.toggle()
+                                self.addEntryMenuViewShowing.toggle()
+                            }
                         }
                     }) {
                         Image("createEntry")
@@ -58,7 +74,9 @@ struct AddEntryMenuView : View {
                     .overlay(Circle().stroke(Color.foreGroundColor, lineWidth: 0.5))
                     .padding(.top, 5)
                 }
+                
                 Spacer()
+            
             }
             .frame(minWidth: 190, idealWidth: nil, maxWidth: 190, minHeight: 70, idealHeight: nil, maxHeight: 70, alignment: .trailing)
             Spacer()
